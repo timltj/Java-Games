9a33a8dfb9c1c191b999103bd7495dff633cd67c
Tim <64743416+timltj@users.noreply.github.com>
2020-05-08 00:51:56 +0800

    Tidied up code.

--- Coronavirus Attack/MapGenerator.java
+++ Coronavirus Attack/MapGenerator.java
@@ -10,8 +10,11 @@ import java.io.File;
  * Encapsulates the generation of the brick map.
  */
 public class MapGenerator {
+    // static final fields
+    private static final Image flag = new Image(new File("redcross.png"));  
+
     // attributes
-    protected int[][] map;
+    protected boolean[][] map;
     protected final int brickWidth;
     protected final int brickHeight;
 
@@ -20,16 +23,10 @@ public class MapGenerator {
      * @param row number of rows in brick map
      * @param col number of columns in brick map
      */
-    public MapGenerator(int row, int col) {
-        // create a 2D array to store bricks
-        map = new int[row][col];
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[0].length; j++) {
-                map[i][j] = 1;
-            }
-        }       
-        brickWidth = 540 / col;
-        brickHeight = 150 / row;
+    public MapGenerator(int row, int col) {        
+        this.map = new boolean[row][col];     
+        this.brickWidth = 540 / col;
+        this.brickHeight = 150 / row;
     }
     
     /**
@@ -37,13 +34,10 @@ public class MapGenerator {
      * @param g java Graphics2D object
      */
     public void draw(Graphics2D g) {
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[0].length; j++) {
-                if (map[i][j] > 0) {
-                    // fill brick color
-                    g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
-                    g.drawImage(flag.image, j * brickWidth + 80, i * brickHeight + 50, null);
-                    // draw brick borders
+        for (int i = 0; i < this.map.length; i++) {
+            for (int j = 0; j < this.map[0].length; j++) {
+                if (!map[i][j]) {
+                    // draw bricks
                     g.setStroke(new BasicStroke(3));
                     g.setColor(Color.BLACK);
                     g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
@@ -54,10 +48,7 @@ public class MapGenerator {
     }
 
     // method to set brick value
-    public void setBrickValue(int value, int row, int col) {
-        map[row][col] = value;
+    public void setBrickValue(int row, int col) {
+        map[row][col] = !map[row][col];
     }
-
-    /* sprites */
-    Image flag = new Image(new File("redcross.png"));     
 }
